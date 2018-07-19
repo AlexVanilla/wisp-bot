07c101aac40287354a57730e5fb5fad0e334fdd6
Alex Villanueva <37161837+AlexVanilla@users.noreply.github.com>
2018-07-19 14:46:38 -0700

    Implement Twitch API

--- index.js
+++ index.js
@@ -36,27 +36,50 @@ client.on('ready', () => {
     });
 })
 
+// TODO: clean up code and implement the STREAMING activity type at some point
+// NOTE: reference to Twitch api
 setInterval(() => {
-    // const activities = []
-    const activityTypes = ['PLAYING', 'LISTENING', 'WATCHING'];
-    const activities = {
-        WATCHING: ['Hanebado', 'Free Season 3', 'Youtube', 'Korean Mukbangs', 'Scryfall Senpai', 'Overlord Season 3', 'Cells At Work', 'Crunchyroll', 'Netflix', 'Udemy Courses'],
-        PLAYING: ['Hearthstone', 'MTGA', 'League of Legends', 'Dota 2', 'Super Smash Bros. Ultimate', 'Fortnite', 'PUBG', 'r/outside', 'Visual Novels', 'the trumpet'],
-        LISTENING: ['Motivational Speeches', 'Country music', 'Dubstep', 'K-pop idol groups', 'J-pop idol groups', 'Soundcloud', 'Scryfall senpai\'s voice', 'Podcasts', 'ClariS', 'Studio Ghibli OST']
-    }
+        // const activities = []
+        const activityTypes = ['PLAYING', 'LISTENING', 'WATCHING'];
+        const activities = {
+            WATCHING: ['Hanebado', 'Free Season 3', 'Youtube', 'Korean Mukbangs', 'Scryfall Senpai', 'Overlord Season 3', 'Cells At Work', 'Crunchyroll', 'Netflix', 'Udemy Courses'],
+            PLAYING: ['Hearthstone', 'MTGA', 'League of Legends', 'Dota 2', 'Super Smash Bros. Ultimate', 'Fortnite', 'PUBG', 'r/outside', 'Visual Novels', 'the trumpet'],
+            LISTENING: ['Motivational Speeches', 'Country music', 'Dubstep', 'K-pop idol groups', 'J-pop idol groups', 'Soundcloud', 'Scryfall senpai\'s voice', 'Podcasts', 'ClariS', 'Studio Ghibli OST']
+        }
 
-    // TODO: Do streaming using Twitch api
-    // const streamingActivities = ['Papa Zero', 'Nairo', 'DisguisedToast', 'Mango', 'Mew2King', ]
+        // const streamingActivities = ['Papa Zero', 'Nairo', 'DisguisedToast', 'Mango', 'Mew2King', ]
 
-    // Randomly choose what activity Wisp bot is doing
-    let currentActivityType = activityTypes[(Math.floor(Math.random() * activityTypes.length - 1) + 1)];
-    let currentActivity = activities[currentActivityType][(Math.floor(Math.random() * activities[currentActivityType].length - 1) + 1)]
+        // Randomly choose what activity Wisp bot is doing
+        let currentActivityType = activityTypes[(Math.floor(Math.random() * activityTypes.length - 1) + 1)];
+        let currentActivity = activities[currentActivityType][(Math.floor(Math.random() * activities[currentActivityType].length - 1) + 1)]
 
-    console.log(currentActivityType, currentActivity);
-    client.user.setActivity(currentActivity, {
-        type: currentActivityType
-    });
-}, 3600000) // Change Activites every hour
+        console.log(currentActivityType, currentActivity);
+
+        // If Wisp bot streaming, use Twitch API to find live streamers
+
+        // https: //api.twitch.tv/helix/streams?first=10&user_login=DisguisedToastHS&user_login=NairoMK&user_login=WagamamaTV
+
+        /*
+        // NOTE: not sure if this curl command will work
+        curl -H `Client-ID: ${process.env.TWITCH_CLIENT_ID}` \
+        -X GET 'https://api.twitch.tv/helix/streams?first=10&user_login=DisguisedToastHS&user_login=NairoMK&user_login=WagamamaTV'
+        */
+
+        // data[].user_id
+
+        // https://api.twitch.tv/helix/users?id=87204022
+        // data[].display_name
+        const streamers = ['DisguisedToastHS', 'ZeRo', 'NairoMK', 'HSdogdog', 'mang0', 'Hungrybox', 'Mew2King', 'Day9tv', 'CONtv'];
+
+        // Append twitch url
+        let options = {
+            type: currentActivityType,
+            url: currentActivityType=='STREAMING'?'https://www.twitch.tv/' + streamers[0] : '' // TODO: array length for streamers
+        }
+
+        client.user.setActivity(currentActivity, options);
+    },
+    60000) // Change Activites every 30 mins
 
 client.on('message', (message) => {
     if (message.content.startsWith("Wisp bot")) {
