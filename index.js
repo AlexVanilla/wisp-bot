15838d0b5f85d5955e69833179af0fa4c32fcc6f
Alex Villanueva <37161837+AlexVanilla@users.noreply.github.com>
2018-07-28 17:01:06 -0700

    Implement Twitch API to index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config(); // Access env variables
 const Discord = require('discord.js');
 const Commando = require('discord.js-commando');
+const axios = require('axios');
 
 const client = new Commando.Client({
     commandPrefix: '!', // What you must type first to use bot command
@@ -41,52 +42,77 @@ client.on('ready', () => {
 // TODO: clean up code and implement the STREAMING activity type at some point
 // NOTE: reference to Twitch api
 setInterval(() => {
-        // const activities = []
-        const activityTypes = ['PLAYING', 'LISTENING', 'WATCHING'];
-        const activities = {
-            WATCHING: ['Hanebado', 'Free Season 3', 'Youtube', 'Korean Mukbangs', 'Scryfall Senpai', 'Overlord Season 3', 'Cells At Work', 'Crunchyroll', 'Netflix', 'Udemy Courses'],
-            PLAYING: ['Hearthstone', 'MTGA', 'League of Legends', 'Dota 2', 'Super Smash Bros. Ultimate', 'Fortnite', 'PUBG', 'r/outside', 'Visual Novels', 'the trumpet'],
-            LISTENING: ['Motivational Speeches', 'Country music', 'Dubstep', 'K-pop idol groups', 'J-pop idol groups', 'Soundcloud', 'Scryfall senpai\'s voice', 'Podcasts', 'ClariS', 'Studio Ghibli OST']
-        }
+    // const activities = []
+    const activityTypes = ['PLAYING', 'LISTENING', 'WATCHING'];
+    const activities = {
+        WATCHING: ['Hanebado', 'Free Season 3', 'Youtube', 'Korean Mukbangs', 'Scryfall Senpai', 'Overlord Season 3', 'Cells At Work', 'Crunchyroll', 'Netflix', 'Udemy Courses'],
+        PLAYING: ['Hearthstone', 'MTGA', 'League of Legends', 'Dota 2', 'Super Smash Bros. Ultimate', 'Fortnite', 'PUBG', 'r/outside', 'Visual Novels', 'the trumpet'],
+        LISTENING: ['Motivational Speeches', 'Country music', 'Dubstep', 'K-pop idol groups', 'J-pop idol groups', 'Soundcloud', 'Scryfall senpai\'s voice', 'Podcasts', 'ClariS', 'Studio Ghibli OST']
+    }
 
-        // const streamingActivities = ['Papa Zero', 'Nairo', 'DisguisedToast', 'Mango', 'Mew2King', ]
+    // const streamingActivities = ['Papa Zero', 'Nairo', 'DisguisedToast', 'Mango', 'Mew2King', ]
 
-        // Randomly choose what activity Wisp bot is doing
-        let currentActivityType = activityTypes[(Math.floor(Math.random() * activityTypes.length - 1) + 1)];
-        let currentActivity = activities[currentActivityType][(Math.floor(Math.random() * activities[currentActivityType].length - 1) + 1)]
+    // Randomly choose what activity Wisp bot is doing
+    // let currentActivityType = activityTypes[(Math.floor(Math.random() * activityTypes.length - 1) + 1)];
+    let currentActivityType = 'STREAMING'; // TODO: implement streaming
+    let currentActivity = null;
 
+    let options = {
+        type: currentActivityType
+    }
 
+    if (currentActivityType !== 'STREAMING') {
+        currentActivity = activities[currentActivityType][(Math.floor(Math.random() * activities[currentActivityType].length - 1) + 1)];
+    } else {
         // If Wisp bot streaming, use Twitch API to find live streamers
-
-        // https: //api.twitch.tv/helix/streams?first=10&user_login=DisguisedToastHS&user_login=NairoMK&user_login=WagamamaTV
-
-        /*
-        // NOTE: not sure if this curl command will work
-        curl -H `Client-ID: ${process.env.TWITCH_CLIENT_ID}` \
-        -X GET 'https://api.twitch.tv/helix/streams?first=10&user_login=DisguisedToastHS&user_login=NairoMK&user_login=WagamamaTV'
-        */
-
-        // data[].user_id
-
-        // https://api.twitch.tv/helix/users?id=87204022
-        // data[].display_name
-        const streamers = ['DisguisedToastHS', 'ZeRo', 'NairoMK', 'HSdogdog', 'mang0', 'Hungrybox', 'Mew2King', 'Day9tv', 'CONtv'];
-
-        // Append twitch url
-        let options = {
-            type: currentActivityType
+        // currentActivityType is 'STREAMING'; build url to make API call
+        const axiosConfig = {
+            // NOTE: Twitch API requires a Client-ID
+            headers: {
+                'Client-ID': process.env.TWITCH_CLIENT_ID
+            }
         }
 
-        if (currentActivityType === 'STREAMING') {
-            options.url = 'https://www.twitch.tv/' + streamers[0] // TODO: array length for streamers
+        const streamers = ['DisguisedToastHS', 'ZeRo', 'NairoMK', 'HSdogdog', 'mang0', 'Hungrybox', 'Mew2King', 'Day9tv', 'CONtv', 'xell'];
+        let axiosUrl = 'https://api.twitch.tv/helix/streams?first=10';
 
+        for (streamer of streamers) {
+            axiosUrl += `&user_login=${streamer}`;
         }
 
-        console.log(currentActivityType, currentActivity, options);
-        client.user.setActivity(currentActivity, options);
-    },
-    6000) // Change Activites every 30 mins
-// 1800000
+        // axios call
+        axios.get(axiosUrl, axiosConfig).then(response => {
+                let liveStreamers = response.data.data;
+                // Randomly choose one of the streamers that is currently live
+                let theChosenOne = liveStreamers[(Math.floor(Math.random() * liveStreamers.length))];
+                // TODO: try to use async/await to make code a bit more readable
+
+                // Get live streamers name name
+
+                // https://api.twitch.tv/helix/users?id="USER_ID"
+                axios.get(`https://api.twitch.tv/helix/users?id=${theChosenOne.user_id}`, axiosConfig).then(response => {
+                    let streamerName = response.data.data[0].display_name;
+                    let login = response.data.data[0].login;
+
+                    client.user.setActivity(streamerName, {
+                        type: 'STREAMING',
+                        url: `https://www.twitch.tv/${login}`
+                    })
+                });
+            })
+            .catch(error => {
+                console.log('ERROR', error.response.data);
+            })
+
+    }
+
+    // NOTE: Wisp bot just doing STREAMING as activity.  Might use other activities as option in the future not sure
+
+    // client.user.setActivity(currentActivity, options);
+
+
+
+}, 3600000) // Change Activites every hour
 
 client.on('message', (message) => {
     if (message.content.startsWith("Wisp bot")) {
